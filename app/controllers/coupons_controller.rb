58b9c4b35427b52d883bbc09c40119b8da1cfc38
Anthony Rurup <tcrurup@gmail.com>
2019-07-18 01:56:31 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -6,7 +6,7 @@ class CouponsController < ApplicationController
     @coupon.store = params[:coupon][:store]
     @coupon.coupon_code = params[:coupon][:coupon_code]
     @coupon.save
-    redirect_to coupons_path(@coupon)
+    redirect_to coupon_path(@coupon)
   end
 
   def index
