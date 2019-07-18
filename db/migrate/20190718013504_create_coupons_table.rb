749f725dd16c30e8d0bf1d73156b03c5feac0766
Anthony Rurup <tcrurup@gmail.com>
2019-07-18 01:36:37 +0000

    Automatically backed up by Learn

--- db/migrate/20190718013504_create_coupons_table.rb
+++ db/migrate/20190718013504_create_coupons_table.rb
@@ -2,7 +2,7 @@ class CreateCouponsTable < ActiveRecord::Migration[5.0]
   def change
     create_table :coupons do |t|
       t.string :coupon_code
-      t.store :chipotle
+      t.string :store
     end
   end
 end
